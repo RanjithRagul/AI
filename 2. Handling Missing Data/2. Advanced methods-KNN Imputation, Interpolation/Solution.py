1fae276163acfdaf97cc19b4e5fb4410e228d372
RanjithRagul <rockranjithkumar@gmail.com>
2025-05-31 13:12:41 +0530

    P-B2: Updated

--- 2. Handling Missing Data/2. Advanced methods-KNN Imputation, Interpolation/Solution.py
+++ 2. Handling Missing Data/2. Advanced methods-KNN Imputation, Interpolation/Solution.py
@@ -1,17 +1,15 @@
 import pandas as pd
+import numpy as np
+from sklearn.impute import KNNImputer
 
-def handle_missing_data(file_path: str) -> None:
+def impute_weather_data(file_path: str, k: int) -> None:
     df = pd.read_excel(file_path)
-    df = df.dropna(how='all', subset=df.columns[1:])
-    df = df.dropna(axis=1, how='all')
-    numeric_cols = df.select_dtypes(include='number').columns
-    for col in numeric_cols:
-        mean_value = df[col].mean()
-        df[col] = df[col].fillna(mean_value)
-
+    df['Temperature'] = df['Temperature'].interpolate(method='linear')
+    knn_imputer = KNNImputer(n_neighbors=k)
+    df[['Temperature', 'Humidity']] = knn_imputer.fit_transform(df[['Temperature', 'Humidity']])
     with pd.ExcelWriter(file_path, engine='openpyxl', mode='a', if_sheet_exists='replace') as writer:
         df.to_excel(writer, sheet_name='result', index=False)
 
 
 if __name__ == "__main__":
-    handle_missing_data(r'C:\AI\2. Handling Missing Data\4. Dropping rows, columns, imputation\sampledata.xlsx')
+    impute_weather_data(r'C:\AI\2. Handling Missing Data\2. Advanced methods-KNN Imputation, Interpolation\sampledata.xlsx')
